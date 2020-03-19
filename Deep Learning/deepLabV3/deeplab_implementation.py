91048acfd03dc1a667ab0254fb032c3249557423
Anunay Bagga <anunayrajesh.bagga2017@vitstudent.ac.in>
2020-03-19 13:05:37 +0530

    Update deeplab_implementation.py

--- Deep Learning/deepLabV3/deeplab_implementation.py
+++ Deep Learning/deepLabV3/deeplab_implementation.py
@@ -1,3 +1,12 @@
+'''
+@inproceedings{deeplabv3plus2018,
+  title={Encoder-Decoder with Atrous Separable Convolution for Semantic Image Segmentation},
+  author={Liang-Chieh Chen and Yukun Zhu and George Papandreou and Florian Schroff and Hartwig Adam},
+  booktitle={ECCV},
+  year={2018}
+}
+'''
+
 import collections
 import os
 #os.environ["CUDA_VISIBLE_DEVICES"]="-1"    
